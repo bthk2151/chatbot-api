2c6e1ea781ce36121e79aceb201cc976dfac4027
Bryan Tan Hoe Kin <bthk2151@gmail.com>
2026-03-23 15:20:17 +0800

    Initial Cloud Run template commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+    const name = process.env.NAME || 'World';
+    res.send(`Hello ${name}!`);
+});
+
+const port = parseInt(process.env.PORT) || 8080;
+app.listen(port, () => {
+    console.log(`chatbot-api: listening on port ${port}`);
+});
