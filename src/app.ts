4801ab87623e49e54ab7a0bf9b5e370aa0a9ca16
Bryan Tan Hoe Kin <bthk2151@gmail.com>
2026-03-28 20:30:40 +0800

    Setup TypeScript

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', (_: express.Request, res: express.Response) => {
     const name = process.env.NAME || 'World';
     res.send(`Hello ${name}!`);
 });
 
-const port = parseInt(process.env.PORT) || 8080;
+const port = parseInt(process.env.PORT!) || 8080;
 app.listen(port, () => {
     console.log(`chatbot-api: listening on port ${port}`);
 });
